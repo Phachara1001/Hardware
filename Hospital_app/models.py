94196cf324409b5f1c986c6339317f0aa200e15b
Phachara1001 <bossyoso789@gmail.com>
2024-01-25 09:37:14 +0700

    แก้ไข

--- Hospital_app/models.py
+++ Hospital_app/models.py
@@ -6,15 +6,15 @@ class d_type(models.Model):
     def __str__(self):
         return f"{self.type_name}"
 
-class drug(models.Model):
-    drug_id = models.AutoField(primary_key=True)
-    drug_name = models.CharField(max_length=255)
-    drug_img = models.ImageField(upload_to='images/drug',null=True)
-    drug_type = models.ForeignKey(d_type, on_delete=models.CASCADE)
-    drug_qty = models.IntegerField()
-    drug_exp = models.DateField()
+class product(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=255)
+    product_img = models.ImageField(upload_to='images/drug',null=True)
+    product_type = models.ForeignKey(d_type, on_delete=models.CASCADE)
+    product_qty = models.IntegerField()
+    product_exp = models.DateField()
     def __str__(self):
-        return f"{self.drug_name} , {self.drug_exp}"
+        return f"{self.product_name} , {self.product_exp}"
     
 class userbuy(models.Model):
     user_id = models.AutoField(primary_key=True)
@@ -28,10 +28,10 @@ class report_buy(models.Model):
     username = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
     tal = models.CharField(max_length=255)
-    drug_name = models.CharField(max_length=255)
-    drug_qty = models.IntegerField()
+    product_name = models.CharField(max_length=255)
+    product_qty = models.IntegerField()
     def __str__(self):
-        return f"{self.username} , {self.drug_qty}"
+        return f"{self.username} , {self.product_qty}"
     
 
     
