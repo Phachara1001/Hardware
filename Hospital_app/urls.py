c2469e03ca7de30c5bf7c25d33ed9685abd3479d
Phachara1001 <bossyoso789@gmail.com>
2024-01-25 09:56:00 +0700

    แก้ไขชื่อไฟล์อีกครั้ง

--- Hospital_app/urls.py
+++ Hospital_app/urls.py
@@ -15,19 +15,19 @@ urlpatterns = [
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('',views.home),
     path('login',views.custom_login),#เรียกใช้ def ชื่อ custom_login
-    path('add_drug',views.add_drug),
+    path('add_product',views.add_product),
     path('add_type',views.add_type),
-    path('manage_drug',views.manage_drug),
+    path('manage_product',views.manage_product),
     path('manage_type',views.manage_type),
-    path('delete/<int:pk>',views.delete_drug),
+    path('delete/<int:pk>',views.delete_product),
     path('delete_type/<int:pk>/', views.delete_type, name='delete_type'),
-    path('edit/<int:pk>',views.edit_drug),#หน้าแก้ไขข้อมูลยา
+    path('edit/<int:pk>',views.edit_product),#หน้าแก้ไขข้อมูลยา
     path('editt/<int:pk>',views.edit_type),#หน้าแก้ไขข้อมูลประเภทยา
-    path('increase_qty/<int:pk>',views.increase_drug),#หน้าเพิ่มจำนวนยา
-    path('decrease_qty/<int:pk>',views.decrease_drug),#หน้าลดจำนวนยา
+    path('increase_qty/<int:pk>',views.increase_product),#หน้าเพิ่มจำนวนยา
+    path('decrease_qty/<int:pk>',views.decrease_product),#หน้าลดจำนวนยา
     path('logout/',views.logout_view),
-    path('buy_drug/', views.buy_drug_view, name='buy_drug'),
-    path('buy_drugu/', views.buy_drug,),
+    path('buy_product/', views.buy_product_view, name='buy_product'),
+    path('buy_productu/', views.buy_product,),
     path('register/', views.register_view, name='register'),
     path('report', views.report_a,),
     
